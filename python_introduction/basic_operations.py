fdf381c76b97cb3c95ac40f70e783285902e21b5
Lebogang Mothata <lebogang.mothata@gmail.com>
2024-09-14 01:46:33 +0200

    Add first python script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1= 10
+number2= 5
+print ("addition of number1 and number2 is",  number1 + number2)
+print ("subtraction of number2 from number1 is", number1 - number2)
+print ("multiplication of number1 and number2 is", number1 * number2)
