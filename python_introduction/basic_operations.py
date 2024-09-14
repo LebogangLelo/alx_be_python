e2fe5ec70256bf92d7dbf925b7d265833242878d
Lebogang Mothata <lebogang.mothata@gmail.com>
2024-09-14 02:13:24 +0200

    Modified

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -2,4 +2,7 @@ number1= 10
 number2= 5
 print ("addition of number1 and number2 is",  number1 + number2)
 print ("subtraction of number2 from number1 is", number1 - number2)
-print ("multiplication of number1 and number2 is", number1 * number2)
+print ("multiplication of number1 and number2 is", print(f"Product of {number1} and {number2} is {product_result}"))
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
+print(f"Product of {number1} and {number2} is {print(f"Product of {number1} and {number2} is {product_result}")}")
