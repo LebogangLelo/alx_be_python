e1212860431c35c80dab1de0f91f32a39789a8e8
Lebogang Mothata <lebogang.mothata@gmail.com>
2024-09-21 15:23:34 +0200

    Daily reminder code

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,22 @@
 task=input("Enter your task: ")
-task_priority=input("Priority (high/medium/low): ").lower()
+priority=input("Priority (high/medium/low): ").lower()
 time_bound=input("Is it time-bound? (yes/no): ").lower()
 
-match task_priority:
+match priority:
     case "high":
         if time_bound == 'no':
             reminder = f"Note: '{task}' is a low priority task. Consider completing it when you have free time."
         else:
-            reminder = f"Reminder: '{task}' is a {task_priority} priority task that requires immediate attention today!"
+            reminder = f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!"
     case "medium":
         if time_bound == 'no':
             reminder = f"Note: '{task}' is a low priority task. Consider completing it when you have free time."
         else:
-             reminder = f"Reminder: '{task}' is a {task_priority} priority task that requires immediate attention today!"
+             reminder = f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!"
     case "low":
         if time_bound == 'no':
             reminder = f"Note: '{task}' is a low priority task. Consider completing it when you have free time."
         else:
-            reminder = f"Reminder: '{task}' is a {task_priority} priority task that requires immediate attention today!"
+            reminder = f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!"
 
 print(reminder)
