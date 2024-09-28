e08c0e008389609359b38fcabbadcd357f592de8
Lebogang <146779455+LebogangLelo@users.noreply.github.com>
2024-09-28 02:27:07 +0200

    Add files via upload

---
+++ arithmetic_operations.py
@@ -0,0 +1,28 @@
+def perform_operation():
+    num1=float(input("Enter the first number: "))
+    num2=float(input("Enter the second number: "))
+    operation=input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+
+    if operation=="add":
+        results=(num1 + num2)
+        
+    elif operation=="subtract":
+        results=(num1 - num2)
+        
+    elif operation=="multiply":
+        results=(num1 * num2)
+        
+    elif operation=="divide":
+        if num2 > 0:
+            results=(num1 / num2)
+        elif num2 == 0:
+            return"Cannot divide by zero"
+        elif num2 < 0:
+            return"Choose a number greater than zero"
+            
+            
+    return results        
+              
+results=perform_operation()
+print(results)
