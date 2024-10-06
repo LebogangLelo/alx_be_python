bd7bb53f8c80a9138f9f83e89f32bd0e151b2e1d
Lebogang Mothata <lebogang.mothata@gmail.com>
2024-10-06 10:27:01 +0200

    bank account script

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,15 +4,15 @@ class BankAccount:
         
     def deposit(self,amount):
             self.account_balance += amount
-            #print(f"Deposited: ${amount}")
+            print(f"Deposited: ${amount}")
     
     def withdraw(self,amount):
          if amount <= self.account_balance:
               self.account_balance -= amount
-              #print(f"Withdrew: ${amount}")
+              print(f"Withdrew: ${amount}")
               return True
          else:
               return False
     
     def display_balance(self):
-         print(f"Current balance: ${self.account_balance}")
+         print(f"Current Balance: ${self.account_balance}")
